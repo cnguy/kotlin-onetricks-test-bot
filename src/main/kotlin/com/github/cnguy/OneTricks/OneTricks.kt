3a490e27c95c1ec7bfaeb4a4404e3019dea7269b
cnguy <chautnguyen96@gmail.com>
2017-12-08 21:51:22 -0800

    Add the MessageListener example

--- src/main/kotlin/com/github/cnguy/OneTricks/OneTricks.kt
+++ src/main/kotlin/com/github/cnguy/OneTricks/OneTricks.kt
@@ -3,9 +3,13 @@ package com.github.cnguy.OneTricks
 import net.dv8tion.jda.core.AccountType.BOT
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.entities.ChannelType
+import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.Event
 import net.dv8tion.jda.core.events.ReadyEvent
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.EventListener
+import net.dv8tion.jda.core.hooks.ListenerAdapter
 import java.io.File
 
 fun main(args: Array<String>) {
@@ -13,6 +17,7 @@ fun main(args: Array<String>) {
     val jda: JDA = JDABuilder(BOT)
             .setToken(token)
             .addEventListener(ReadyListener())
+            .addEventListener(MessageListener())
             .buildBlocking()
 }
 
@@ -20,6 +25,19 @@ class ReadyListener: EventListener {
     override fun onEvent(event: Event) {
         if (event is ReadyEvent) {
             println("API is ready!")
+        }
+    }
+}
+
+class MessageListener: ListenerAdapter() {
+    override fun onMessageReceived(event: MessageReceivedEvent?) {
+        if (event!!.isFromType(ChannelType.PRIVATE)) {
+            println("[PM] ${event.author.name}: ${event.message.content}")
+        } else {
+            println(
+                    "[${event.guild.name}][${event.textChannel?.name}]" +
+                    " ${event.member.effectiveName}: ${event.message?.content}"
+            )
         }
     }
 }
